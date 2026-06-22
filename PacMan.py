8f5b4776dd27de56dbd73c809e5b3f5800da931f
PU-OL <pu-ol@gmx.de>
2026-06-22 12:21:56 +0200

    Add function to "eat" apples

--- PacMan.py
+++ PacMan.py
@@ -1,29 +1,86 @@
 import pygame
+import random
 
 pygame.init()
-pygame.display.set_caption("Pac Man")
+pygame.display.set_caption("Pac Man Apples: 0")
 screen = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
 running = True
 dt = 0
+generator = 0
+next_generator = generator
+apple_counter = 0
 
-bloon_pos = pygame.Vector2(screen.get_width(), screen.get_height())
-screen.fill("yellow")
+pac_pos = pygame.Vector2(screen.get_width()/2, screen.get_height()/2)
+pac_picture = pygame.image.load("pictures/PacMan_open.png")
+pac_picture = pygame.transform.scale(pac_picture, (40, 40))
+screen.fill("black")
+
+ghost_pos = {
+    "red": pygame.Vector2(screen.get_width()/2, screen.get_height()/2),
+    "pink": pygame.Vector2(screen.get_width()/2, screen.get_height()/2),
+    "blue": pygame.Vector2(screen.get_width()/2, screen.get_height()/2),
+    "orange": pygame.Vector2(screen.get_width()/2, screen.get_height()/2),
+}
+
+apple_pos = {}
+apple_del = []
 
 while running:
+    generator += 1
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    pygame.draw.circle(screen, "red", bloon_pos, 40)
-
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_BACKSPACE]:
-        screen.fill("black")
     if keys[pygame.K_w]:
-        screen.fill("green")
+        pac_pos.y -= 2
+    if keys[pygame.K_s]:
+        pac_pos.y += 2
+    if keys[pygame.K_a]:
+        pac_pos.x -= 2
+    if keys[pygame.K_d]:
+        pac_pos.x += 2
+    if keys[pygame.K_BACKSPACE]:
+        pac_pos = pygame.Vector2(screen.get_width()/2, screen.get_height()/2)
 
+    if(len(apple_pos) < 15 and next_generator < generator):
+        next_generator = next_generator + 100
+        new_cord = True
+        while(new_cord):
+            random_x_pos = random.randint(0,screen.get_width()-5)
+            random_y_pos = random.randint(0,screen.get_height()-5)
+            if(random_x_pos not in apple_pos):
+                new_cord = False
+
+        apple_name = "Apple_" + str(len(apple_pos))
+        apple_pos[apple_name] = pygame.Vector2(random_x_pos, random_y_pos)
+
+    screen.blit(pac_picture, pac_pos)
+
+    count = 0
+    for i in apple_pos:
+        count += 1
+        apple_x_reach = apple_pos[i].x - 50
+        apple_y_reach = apple_pos[i].y - 50
+
+        apple_x_reach2 = apple_pos[i].x + 50
+        apple_y_reach2 = apple_pos[i].y + 50
+
+        if(pac_pos.x > apple_x_reach and pac_pos.x < apple_x_reach2 and pac_pos.y > apple_y_reach and pac_pos.y < apple_y_reach2):
+            apple_counter += 1
+            pygame.display.set_caption("Pac Man Apples: " + str(apple_counter))
+            apple_del.append(i)
+
+    for i in apple_del:
+        del apple_pos[apple_del[0]]
+    apple_del.clear()
+
+    for i in apple_pos:
+        pygame.draw.circle(screen, "red", apple_pos[i], 5)
 
     pygame.display.flip()
     dt = clock.tick(60) / 1000.0
+
 pygame.quit()
